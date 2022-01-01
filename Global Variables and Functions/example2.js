f26d002612e14a9d126dfaf7b845e158d9d4ff6e
Hui Hong Zheng <86531095+HuiHongOP@users.noreply.github.com>
2022-01-01 11:58:49 -0500

    example2

---
+++ Global Variables and Functions/example2.js
@@ -0,0 +1,7 @@
+//Declared function named “sumTwoArguments” with 2 parameters.
+function sumTwoArguments(arg1,arg2){
+	console.log(arg1+arg2);
+}
+
+//Calling the function to sum of the two passed values 
+sumTwoArguments(10,10); // Note: This will print out 20.

6ea9598d245a47e9de9194c0a14f3edddbe4ba0e
Hui Hong Zheng <86531095+HuiHongOP@users.noreply.github.com>
2022-01-01 11:52:54 -0500

    Example1

---
+++ Global Variables and Functions/Example1.js
@@ -0,0 +1,10 @@
+let stringGreet = "Welcome to JavaScript!";   //global variable
+
+// JavaScript function named greet that prints out “Welcome to JavaScript”
+function greet(){
+
+	console.log(stringGreet);
+}
+
+//Calling the function
+greet();
